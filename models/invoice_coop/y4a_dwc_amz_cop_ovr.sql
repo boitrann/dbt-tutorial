9e3b65d6a522c93654b90cb8f34dcb8c541a2c6e
YES4ALL\dattran <dattran@yes4all.com>
2024-09-29 18:19:18 +0700

    '..'

---
+++ models/invoice_coop/y4a_dwc_amz_cop_ovr.sql
@@ -0,0 +1,16 @@
+
+{{ config(materialized='table') }}
+
+SELECT 
+    to_timestamp(run_date, 'YYYY-MM-DD HH24:MI:SS') as withdraw_time,
+    invoice_number as invoice_id,
+    to_timestamp(invoice_date, 'YYYY-MM-DD') as invoice_date,
+    agreement_number as agreement_id,
+    agreement_title as agreement_title,
+    original_invoice_amount as original_balance,
+    currency as currency,
+    country as country,
+    backup_report_input as backup_report_input,
+    invoice_line_type as invoice_line_type,
+    funding_type as funding_type
+FROM {{ source('coop', 'y4a_dwc_amz_avc_cop_ovr') }}
